099280eb6bbbf31e3ed55d73d8ddb673504fade4
Mason Bybee <bybeemason@gmail.com>
2024-01-25 16:02:16 -0600

    added step1

---
+++ step1.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+file = process.argv[2];
+
+fs.readFile(file, "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR!", err);
+    process.exit(1);
+  }
+  console.log(data);
+});
